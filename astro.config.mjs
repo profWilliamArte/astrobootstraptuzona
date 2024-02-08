c4551ceb639ca22826d47daa7b911c0b36a6e42f
profWilliamArte <arteagawilliam13@hotmail.com>
2024-02-08 07:55:29 -0400

    2er Commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,5 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
     site: 'https://profwilliamarte.github.io',
     base: '/astrobootstraptuzona',
+    trailingSlash: "never"
 });
